6e9bbbbfb350780b8274acaf1abd02b9c48a90e0
shuharib0t <shuharib0t@gmail.com>
2023-05-29 12:24:08 -0300

    fix avatar bug

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -31,13 +31,16 @@ export function Profile() {
   }
 
   async function handleUpdate() {
-    const user = {
+    const updated = {
       name,
       email,
       old_password: passwordOld,
       password: passwordNew,
     };
-    await updateProfile({ user, avatarFile });
+
+    const userUpdated = Object.assign(user, updated);
+
+    await updateProfile({ user: userUpdated, avatarFile });
   }
 
   function handleChangeAvatar(event) {
