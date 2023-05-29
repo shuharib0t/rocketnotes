a19323b6188611c1f678e57b6c2a4a0521ced5b5
shuharib0t <shuharib0t@gmail.com>
2023-05-29 12:29:14 -0300

    fix logout bug

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,6 @@
 import { RiShutDownLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
+
 import { Container, Profile, Logout } from "./styles";
 
 import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
@@ -9,9 +11,17 @@ import { useAuth } from "../../hooks/auth";
 export function Header() {
   const { signOut, user } = useAuth();
 
+  const navigate = useNavigate();
+
   const avatarUrl = user.avatar
     ? `${api.defaults.baseURL}/files/${user.avatar}`
     : avatarPlaceholder;
+
+  function handleSignOut() {
+    navigate("/");
+    signOut();
+  }
+
   return (
     <Container>
       <Profile to="/profile">
@@ -22,7 +32,7 @@ export function Header() {
           <strong>{user.name}</strong>
         </div>
       </Profile>
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
